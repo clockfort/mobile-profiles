1b60811a3a2a389cb177b41be4cd1b23a308a2d2
Chris Lockfort <clockfort@csh.rit.edu>
2012-03-10 18:17:04 -0500

    new more simple sanitization method

--- HTMLSanitize.hs
+++ HTMLSanitize.hs
@@ -2,16 +2,10 @@ module HTMLSanitize where
 
 	--Awwww yeah, sanitizing user-editable fields with folds and list comprehensions.
 	-- This is how the web was meant to be.
-	sanitize str = stripEscapists $ stripTags str
+	sanitize str = foldl escapist [] str
 	
-	stripTags = res . foldl update (Right "")
-		where
-			res (Left s)  = s
-			res (Right s) = s
-
-			update (Left c) '>'  = Right c
-			update (Left c) _    = Left c
-			update (Right c) '<' = Left c
-			update (Right c) n   = Right (c ++ [n])
-
-	stripEscapists str = [ ch | ch <- str, ch `notElem` "\\<>\"&" ]
+	escapist xs x 	| x == '<' = xs ++ "&lt;"
+					| x == '>' = xs ++ "&gt;"
+					| x == '"' = xs ++ "&quot;"
+					| x == '&' = xs ++ "&amp;"
+					| otherwise = xs ++ [x]
